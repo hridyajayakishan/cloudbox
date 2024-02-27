de4c2c5b11f0bf7d86d78c20c429c2a4efda89e6
Hridya <hridyajayakishan2002@gmail.com>
2024-02-27 15:14:33 +0400

    initial commit

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
+# Register your models here.
 from django.contrib import admin
+from store.models import Category,Product,Size
+
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Size)
